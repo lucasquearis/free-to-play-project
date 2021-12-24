402fd78affb0f2c39abebb8b74012e38baa3768b
lucasquearis <lucasquearis@hotmail.com>
2021-12-24 14:53:54 -0300

    adiciona logica para renderizar ou nao os botoes de pagination

--- src/Components/CardsDisplay/Pagination.jsx
+++ src/Components/CardsDisplay/Pagination.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./Pagination.css";
 // https://www.youtube.com/watch?v=v91BLoapVDw&t=1256s&ab_channel=ViniciusDacal
+// https://stackoverflow.com/questions/62954765/how-to-do-conditional-rendering-according-to-screen-width-in-react/62954922#62954922
 
 const MAX_ITEMS = 9;
 const MAX_LEFT = (MAX_ITEMS - 1) / 2;
 
 const Pagination = ({ limit, total, offset, setOffSet }) => {
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  const maxWidthToRenderPagesNumber = 800;
   const currentPage = offset ? offset / limit + 1 : 1;
   const totalPages = Math.ceil(total / limit);
   const firstPage = Math.max(currentPage - MAX_LEFT, 1);
+
+  useEffect(() => {
+    const handleResizeWindow = () => setPageWidth(window.innerWidth);
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
 
   const handleClick = (page) => {
     setOffSet((page - 1) * limit);
@@ -26,26 +37,30 @@ const Pagination = ({ limit, total, offset, setOffSet }) => {
           Previous
         </button>
       </li>
-      {Array.from({ length: Math.min(MAX_ITEMS, totalPages) })
-        .map((_, index) => index + firstPage)
-        .map((page) => {
-          if (page <= totalPages) {
-            return (
-              <li key={page}>
-                <button
-                  className={
-                    page === currentPage
-                      ? "btn btn-secondary page-number-btn"
-                      : "btn btn-light page-number-btn"
-                  }
-                  onClick={() => handleClick(page)}
-                >
-                  {page}
-                </button>
-              </li>
-            );
-          }
-        })}
+      {pageWidth > maxWidthToRenderPagesNumber ? (
+        Array.from({ length: Math.min(MAX_ITEMS, totalPages) })
+          .map((_, index) => index + firstPage)
+          .map((page) => {
+            if (page <= totalPages) {
+              return (
+                <li key={page}>
+                  <button
+                    className={
+                      page === currentPage
+                        ? "btn btn-secondary page-number-btn"
+                        : "btn btn-light page-number-btn"
+                    }
+                    onClick={() => handleClick(page)}
+                  >
+                    {page}
+                  </button>
+                </li>
+              );
+            }
+          })
+      ) : (
+        <button className="btn btn-light page-number-btn">{currentPage}</button>
+      )}
       <li>
         <button
           className="btn btn-secondary previous-next"
